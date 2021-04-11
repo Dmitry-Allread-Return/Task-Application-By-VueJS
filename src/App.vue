ec6a1da2993e21a4d9ba230b05782052c96c3b07
Dmitry <dvarnavskikh01@mail.ru>
2021-04-11 17:55:41 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header text="Vue crash" @toggle-add-task="toggleAddTask" :showAddTask="showAddTask" />
+    
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Footer,
+    Header,
+  },
+  data() {
+    return {
+      showAddTask: false
+    }
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,20 @@
+<template>
+	<div v-for="(task, index) of tasks" :key="task.id">
+		<Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" :index="index" />
+	</div>
+</template>
+
+<script>
+import Task from './Task'
+export default {
+	name: 'Tasks',
+	props: {
+		tasks: Array
+	},
+	components: {
+		Task
+	},
+	emits: ['delete-task', 'toggle-reminder'],
+
+}
+</script>
